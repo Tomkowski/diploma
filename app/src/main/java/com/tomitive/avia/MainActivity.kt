dd235cb3c76063395d30731a873b8b4f513dd7a8
Tomek <siemtomek@gmail.com>
2020-07-16 12:53:37 +0200

    chessboard fav icon

--- app/src/main/java/com/tomitive/avia/MainActivity.kt
+++ app/src/main/java/com/tomitive/avia/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.tomitive.avia
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
@@ -13,6 +15,7 @@ import com.tomitive.avia.model.TimeFormatManager
 
 
 class MainActivity : AppCompatActivity() {
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -40,4 +43,17 @@ class MainActivity : AppCompatActivity() {
             TimeFormatManager(format, timeZone)
 
     }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
+    }
+
 }
